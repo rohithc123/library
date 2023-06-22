7b95432772006e80e8b4a559c7a3c052a30eba24
Rohith C <rohithc.eec.cseb@gmail.com>
2023-06-22 16:57:52 +0530

    Mitigated navbar not being sticky

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,31 @@
-import React from 'react'
-import "../styles/navbar.css"
-import search from "../images/search.png"
-import cart from "../images/cart.png"
-import profile from "../images/profile.png"
+import React from "react";
+import "../styles/navbar.css";
+import search from "../images/search.png";
+import cart from "../images/cart.png";
+import profile from "../images/profile.png";
 
 function Navbar() {
   return (
     <div>
       <div className="header">
         <div className="website-name">
-          <div className="style-colour">
-          Information&nbsp;
-          </div>
-          
+          <div className="style-colour">Information&nbsp;</div>
           Oasis
         </div>
         <div className="center-section">
-          <div>
-          &nbsp;Rent&nbsp;
-          </div>
-          <div>
-            How it works&nbsp;
-          </div>
-          <div>
-            About us&nbsp;
-          </div>
+          <div>&nbsp;Rent&nbsp;</div>
+          <div>How it works&nbsp;</div>
+          <div>About us&nbsp;</div>
         </div>
 
         <div className="logo-section">
-          <img src={search} alt="Search icon" className="logo-search"/>
-          <img src={cart} alt="Checkout icon"  className="logo-cart"/>
-          <img src={profile} alt="Login icon"  className="logo-profile"/>
-
+          <img src={search} alt="Search icon" className="logo-search" />
+          <img src={cart} alt="Checkout icon" className="logo-cart" />
+          <img src={profile} alt="Login icon" className="logo-profile" />
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
