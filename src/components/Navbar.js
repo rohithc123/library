b2d5a2846f94c21ff3264f8823abfe8945ebd8da
Rohith C <rohithc.eec.cseb@gmail.com>
2023-06-22 12:31:23 +0530

    Added basic navbar contruction

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../styles/navbar.css"
+
+function Navbar() {
+  return (
+    <div>
+      <div className="header">
+        <div className="website-name">
+         Information Oasis
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
