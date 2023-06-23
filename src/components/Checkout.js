a0256a2f2fcc121cd3f315ad668e247263e79315
Rohith C <rohithc.eec.cseb@gmail.com>
2023-06-23 18:48:29 +0530

    Added calculating basket total

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,9 @@
 import React from "react";
 import CheckoutCard from "./CheckoutCard";
 import "../styles/checkout.css";
-import icon from "../images/bookcover.jfif";
+import CurrencyFormat from "react-currency-format";
 
+import icon from "../images/bookcover.jfif";
 
 function Checkout() {
   return (
@@ -75,24 +76,44 @@ function Checkout() {
         </div>
         <div className="order-details">
           <div className="order-header">ORDER DETAILS</div>
-          <div className="order-indi-section" style={{marginTop:"3.8%"}}>
+          <div className="order-indi-section" style={{ marginTop: "3.8%" }}>
             <div className="order-left-section">PRODUCT</div>
-            <div className="order-right-section" style={{fontWeight:"700"}}>SUBTOTAl</div>
+            <div className="order-right-section" style={{ fontWeight: "700" }}>
+              SUBTOTAl
+            </div>
           </div>
-          <CheckoutCard title='First book' price='300' image={icon}/>
-          <CheckoutCard title='Second book' price='400' image={icon}/>
-          <CheckoutCard title='Second book' price='500' image={icon}/>
-          <CheckoutCard title='Second book' price='600' image={icon}/>
+          <CheckoutCard title="First book" price="300" image={icon} />
+          <CheckoutCard title="Second book" price="400" image={icon} />
+          <CheckoutCard title="Second book" price="500" image={icon} />
+          <CheckoutCard title="Second book" price="600" image={icon} />
 
-          <div className="order-indi-section" style={{backgroundColor:"rgba(161, 161, 161,0.05)"}} >
-            <div className="order-left-section">SUBTOTAL</div>
-            <div className="order-right-section">&#8377; 1567</div>
-          </div>
-          <div className="order-indi-section" style={{backgroundColor:"rgba(161, 161, 161,0.05)"}} >
+          <CurrencyFormat
+            renderText={(value) => (
+              <div
+                className="order-indi-section"
+                style={{ backgroundColor: "rgba(161, 161, 161,0.05)" }}
+              >
+                <div className="order-left-section">
+                  SUBTOTAL
+                  <div style={{fontWeight:"400"}}>(0 items): </div>
+                </div>
+                <div className="order-right-section">&#8377; 1567</div>
+              </div>
+            )}
+            decimalScale={2}
+            // value={GetBasketTotal(basket)}
+            displayType={"text"}
+            thousandSeparator={true}
+            prefix={"₹"}
+          />
+          <div
+            className="order-indi-section"
+            style={{ backgroundColor: "rgba(161, 161, 161,0.05)" }}
+          >
             <div className="order-left-section">SHIPPING</div>
             <div className="order-right-section">&#8377;75</div>
           </div>
-          <div className="order-indi-section"  >
+          <div className="order-indi-section">
             <div className="order-left-section">TOTAL</div>
             <div className="order-right-section">&#8377;1642</div>
           </div>
