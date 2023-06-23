0b42c11b2a42a5c3af999b957f44196a67b39f9e
Rohith C <rohithc.eec.cseb@gmail.com>
2023-06-23 20:18:43 +0530

    Mitigated double rendering of card component while adding

--- src/index.js
+++ src/index.js
@@ -2,17 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import StateProvider from "./components/Stateprovider";
-import reducer,{ initialState } from "./components/Reducer";
+import Reducer,{ initialState } from "./components/Reducer";
 import reportWebVitals from "./reportWebVitals";
 import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <StateProvider initialState={initialState} reducer={reducer}>
+    <StateProvider initialState={initialState} reducer={Reducer}>
       <App />
     </StateProvider>
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
