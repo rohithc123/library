ef47c9918296f04617cfa457212aabde741d0c05
Rohith C <rohithc.eec.cseb@gmail.com>
2023-06-23 19:24:18 +0530

    Mitigated error while importing

--- src/components/Stateprovider.js
+++ src/components/Stateprovider.js
@@ -2,11 +2,15 @@ import React,{createContext , useContext , useReducer} from "react";
 
 export const StateContext = createContext();
 
-export const StateProvider = ({reducer,initialState,children}) =>(
+export const useStateValue = () => useContext(StateContext);
+
+
+const StateProvider = ({reducer,initialState,children}) =>(
     <StateContext.Provider value={useReducer(reducer,initialState)}>
         {children}
     </StateContext.Provider>
 );
 
 //pulling info from data layer
-export const useStateValue = () => useContext(StateContext);
+
+export default StateProvider;
