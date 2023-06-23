edd061d5680d61b0be3b8d087f083a5e2284877f
Rohith C <rohithc.eec.cseb@gmail.com>
2023-06-23 14:15:41 +0530

    Passing info like author,title etc. to card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,22 +2,24 @@ import React from "react";
 import "../styles/card.css";
 import icon from "../images/bookcover.jfif";
 
-const Card = () => {
+function Card({title,author,price}) {
   return (
-    <>
+    <div>
       <div className="card">
         <div className="card-image">
-          <div className="card-child card-amount">&#8377; 1567</div>
+          <div className="card-child card-amount">&#8377; {price}</div>
           <img src={icon} alt="" className="card-child image" />
         </div>
         <div className="card-info">
-          <div className="card-book-name">Book name</div>
-          <div className="card-author">Book author
-          <button>Add to Cart</button></div>
+          <div className="card-book-name">{title}</div>
+          <div className="card-author">
+            {author}
+            <button className="card-add-cart">Add to Cart</button>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
-};
+}
 
 export default Card;
