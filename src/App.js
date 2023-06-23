77fe4ce2616a822097c478e2632ea53c220e2e74
Rohith C <rohithc.eec.cseb@gmail.com>
2023-06-23 14:16:07 +0530

    Static passing info(will modify it with api)

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
-import Navbar from "./components/Navbar"
-import Homepage from "./components/Homepage"
-import Working from "./components/Working"
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Homepage from "./components/Homepage";
+import Working from "./components/Working";
+import Bookfinder from "./components/Bookfinder";
+import Checkout from "./components/Checkout";
+import Card from "./components/Card";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Search from "./components/Search";
 
 function App() {
   return (
     <div>
-      
-        <Navbar/>
-        <Homepage/>
-        <Working/>
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Navbar />
+                <Homepage />
+                <Working />
+                <Card title="home-page-book-1" author="phoenix" price="900" />
+                <Card title="home-page-book-2" author="dragon" price="300" />
+                <Card title="home-page-book-3" author="jisoo" price="200" />
+                <Card title="home-page-book-4" author="jett" price="399" />
+                <Card title="home-page-book-5" author="raze" price="499" />
+                <Card title="home-page-book-6" author="brim" price="800" />
+              </>
+            }
+          />
+          <Route
+            path="/search"
+            element={
+              <>
+                <Navbar />
+                <Search />
+                <Homepage />
+                <Working />
+                <Card title="home-page-book-1" author="phoenix" price="900" />
+                <Card title="home-page-book-2" author="dragon" price="300" />
+                <Card title="home-page-book-3" author="jisoo" price="200" />
+                <Card title="home-page-book-4" author="jett" price="399" />
+                <Card title="home-page-book-5" author="raze" price="499" />
+                <Card title="home-page-book-6" author="brim" price="800" />
+              </>
+            }
+          />
+          <Route
+            path="/checkout"
+            element={
+              <>
+                <Navbar />
+                <Checkout />
+              </>
+            }
+          />
+        </Routes>
+      </Router>
+
+      {/* <Bookfinder/> */}
     </div>
   );
 }
