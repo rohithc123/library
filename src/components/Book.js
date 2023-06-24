6f546e542e1504182a3240fb1912e5c90b69a5d0
Rohith C <rohithc.eec.cseb@gmail.com>
2023-06-24 18:41:46 +0530

    Temporary card for testing API

--- src/components/Book.js
+++ src/components/Book.js
@@ -10,19 +10,19 @@ const Book = ({ book }) => {
         let cover_img =
           item.volumeInfo.imageLinks.smallThumbnail &&
           item.volumeInfo.imageLinks.smallThumbnail;
-        
-        let pages=item.volumeInfo.pageCount;
-        let author=item.volumeInfo.authors;
-        let amount=pages;
 
-        if (cover_img != undefined ) {
+        let pages = item.volumeInfo.pageCount;
+        let author = item.volumeInfo.authors;
+        let amount = pages;
+
+        if (cover_img != undefined) {
           return (
-            <div className="card">
-              <div className="card-bg">
-                <img src={cover_img} alt="" className="book-image" />
-                <div className="book-name">{item.volumeInfo.title}</div>
-                <div className="author">{author}</div>
-                <div className="book-price">&#8377;{pages}</div>
+            <div className="b-card">
+              <div className="b-card-bg">
+                <img src={cover_img} alt="" className="b-book-image" />
+                <div className="b-book-name">{item.volumeInfo.title}</div>
+                <div className="b-author">{author}</div>
+                <div className="b-book-price">&#8377;{pages}</div>
               </div>
             </div>
           );
